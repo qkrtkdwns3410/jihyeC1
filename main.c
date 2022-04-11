5857c0c9c474113373aaba88c684b82e5033a7d3
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-04-12 00:11:57 +0900

    22 04 12

--- main.c
+++ main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 void sale_score_report(int per_person_score_list[5][4], char person_name[5][20]) {
     printf("======================================================\n");
     printf("        판매사원  1/4분기  2/4분기  3/4분기  4/4분기  총합        \n");
     printf("======================================================\n");
     for (int i = 0; i < 5; i++) {
-        printf("%s %d %d %d %d %d %d",)
+        int sum_of = 0;
+        for (int j = 0; j < 4; j++) {
+            sum_of += per_person_score_list[i][j];
+        }
+        printf("%s %ld %ld %d %d %d %d\n", &person_name[i], per_person_score_list[i][0], per_person_score_list[i][1], per_person_score_list[i][2],
+               per_person_score_list[i][3], sum_of);
+        
+        
     }
 }
 
@@ -15,6 +23,7 @@ int main() {
     char person_name[5][20] = {"KIM", "LEE", "PARK", "SONG", "CHOI"};
     int per_person_score_list[5][4] = {{34000, 56000, 23000, 78000},
                                        {23000, 65000, 18000, 94000},
+                                       {18000, 52000, 23500, 73950},
                                        {25000, 34000, 19000, 68000},
                                        {25000, 38000, 26000, 64000}};
 //    for (int i = 0; i < 5; i++) {
@@ -24,7 +33,7 @@ int main() {
 //        }
 //
 //    }
-    sale_score_report(per_person_score_list);
+    sale_score_report(per_person_score_list, person_name);
     
     return 0;
 }
